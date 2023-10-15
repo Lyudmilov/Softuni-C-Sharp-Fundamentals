7a822db7130db710571d5c33cc8cb2a6b0269b58
Dimitar Georgiev <dimitar.lyudmilov@gmail.com>
2023-10-16 00:57:26 +0300

    adding project in solution

--- Lab Methods/3.Calculations/Program.cs
+++ Lab Methods/3.Calculations/Program.cs
@@ -4,7 +4,64 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+             – "add", "multiply", "subtract", "divide"
+             */
+            /*
+               subtract
+               5
+               4
+               divide
+               8
+               4   
+             */
+
+            
+            string mathOperation = Console.ReadLine();
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            MathOperation(mathOperation, num1, num2);
         }
+
+        private static void MathOperation(string mathOperation, int num1, int num2)
+        {
+            switch (mathOperation)
+            {
+                case "add":
+                    Add(num1, num2);
+                    break;
+                case "multiply":
+                    Multiply(num1, num2);
+                    break;
+                case "subtract":
+                    Subtract(num1, num2);
+                    break;
+                case "divide":
+                    Divide(num1, num2);
+                    break;
+            }
+        }
+
+        static void Divide(int num1, int num2)
+        {
+            Console.WriteLine(num1 / num2);
+        }
+
+        static void Subtract(int num1, int num2)
+        {
+            Console.WriteLine(num1 - num2);
+        }
+
+        static void Multiply(int num1, int num2)
+        {
+            Console.WriteLine(num1 * num2);
+        }
+
+        static void Add(int num1, int num2)
+        {
+            Console.WriteLine(num1 + num2);
+        }
+
     }
 }
---
+++ Lab Methods/4.PrintingTriangle/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _4.PrintingTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            PrintTriangle(n);
+
+        }
+
+        private static void PrintTriangle(int n)
+        {
+            PrintFirstHalfOfTriangle(n);
+            PrintSecondHalfOfTriangle(n);
+        }
+
+        private static void PrintSecondHalfOfTriangle(int n)
+        {
+            for (int rol = n - 1; rol >= 1; rol--)
+            {
+                PrintColumn(rol);
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void PrintFirstHalfOfTriangle(int n)
+        {
+            for (int rol = 1; rol <= n; rol++)
+            {
+                PrintColumn(rol);
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void PrintColumn(int rol)
+        {
+            for (int col = 1; col <= rol; col++)
+            {
+                Console.Write(col + " ");
+            }
+        }
+    }
+}
