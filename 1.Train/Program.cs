570164816adce81bc661a1269cf733a7c3955534
Dimitar Georgiev <dimitar.lyudmilov@gmail.com>
2023-10-15 20:50:12 +0300

    add

---
+++ 1.Train/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _1.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] peopleInWagon = new int[n];
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                peopleInWagon[i] = int.Parse(Console.ReadLine());
+                sum += peopleInWagon[i];
+            }
+
+            Console.WriteLine(string.Join(" ", peopleInWagon));
+            Console.WriteLine(sum);
+        }
+    }
+}
