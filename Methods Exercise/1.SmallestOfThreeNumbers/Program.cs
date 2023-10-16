cc375c7e7357e7aa44d24175101a238aaaa46224
Dimitar Georgiev <dimitar.lyudmilov@gmail.com>
2023-10-17 02:48:59 +0300

    tasks

--- Methods Exercise/1.SmallestOfThreeNumbers/Program.cs
+++ Methods Exercise/1.SmallestOfThreeNumbers/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace _1.SmallestOfThreeNumbers
+﻿using System;
+using System.Net.WebSockets;
+
+namespace _1.SmallestOfThreeNumbers
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /* List<int> num = new List<int>();
+
+             while (true)
+             {
+
+                 if (int.TryParse(Console.ReadLine(), out int nums))
+                 {
+                     num.Add(nums);
+                 }
+                 else
+                 {
+                     break;
+                 }
+             }
+
+             int min = num.Min();
+             Console.WriteLine(min);*/
+            List<int> num = PrintSmallestInt();
+            Console.WriteLine(num.Min());
+        }
+
+        static List<int> PrintSmallestInt()
+        {
+            List<int> num = new List<int>();
+
+            while (true)
+            {
+
+                if (int.TryParse(Console.ReadLine(), out int nums))
+                {
+                    num.Add(nums);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            //int min = num.Min();
+            return num;
+
         }
     }
 }
