8ca7de48821270cc8111f0c6dc382be563d740b9
Dimitar Georgiev <dimitar.lyudmilov@gmail.com>
2023-10-16 16:07:28 +0300

    adding tasks

--- Lab Methods/8.MathPower/Program.cs
+++ Lab Methods/8.MathPower/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(PrintMathPower(num1, num2));
+            
+        }
+
+        private static double PrintMathPower(double num1, double num2)
+        {
+            double result = Math.Pow(num1, num2);
+            return result;
         }
     }
 }
