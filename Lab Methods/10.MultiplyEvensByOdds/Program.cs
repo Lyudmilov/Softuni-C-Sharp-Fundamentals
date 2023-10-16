3c568b0f74c54c10b4f9132fcb960326b53eda8b
Dimitar Georgiev <dimitar.lyudmilov@gmail.com>
2023-10-17 00:28:43 +0300

    adding tasks

---
+++ Lab Methods/10.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using static System.Net.Mime.MediaTypeNames;
+using Console = System.Console;
+
+namespace _10.MultiplyEvensByOdds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Math.Abs(int.Parse(Console.ReadLine()));
+            int multiplySumsEvenOdd = FindEvenSum(n) * FindOddSum(n);
+            Console.WriteLine(multiplySumsEvenOdd);
+            Console.WriteLine(FindEvenSum(n));
+            Console.WriteLine(FindOddSum(n));
+
+        }
+
+        static int FindEvenSum(int num)
+        {
+            int sumEven = 0;
+
+            while (num > 0)
+            {
+                int next = num % 10;
+
+                /*if (next % 2 == 0)
+                {
+                    sumEven += next;
+                }*/
+                sumEven += next % 2 == 0 ? next : 0;
+
+                num /= 10;
+            }
+            return sumEven;
+        }
+
+        static int FindOddSum(int num)
+        {
+            int sumOdd = 0;
+
+            while (num > 0)
+            {
+                int next = num % 10;
+
+                /*if (nextN % 2 == 0)
+                {
+                    sumOdd += nextN;
+                }*/
+                sumOdd += next % 2 != 0 ? next : 0;
+
+                num /= 10;
+            }
+            return sumOdd;
+        }
+    }
+}
--- Lab Methods/9.GreaterOfTwoValues/Program.cs
+++ Lab Methods/9.GreaterOfTwoValues/Program.cs
@@ -4,7 +4,57 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+             int
+             2
+             16
+             char
+             a
+             z
+             string
+             aaa
+             bbb               
+             */
+            string input = Console.ReadLine();
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            
+            switch (input)
+            {
+                case "int":
+                    int firstInt = int.Parse(input1);
+                    int secondInt = int.Parse(input2);
+                    int resultInt = GetMax(firstInt, secondInt);
+                    Console.WriteLine(resultInt);
+                    break;
+                case "char":
+                    char firstChar = char.Parse(input1);
+                    char secondChar = char.Parse(input2);
+                    char resultCh = GetMax(firstChar, secondChar);
+                    Console.WriteLine(resultCh);
+                    break;
+                case "string":
+                    string firstStr = input1;
+                    string secondStr = input2;
+                    string resultStr = GetMax(firstStr, secondStr);
+                    Console.WriteLine(resultStr);
+                    break;
+            }
+        }
+
+        static string GetMax(string a, string b)
+        {
+            return string.Compare(a, b) > 0 ? a : b;
+        }
+
+        static char GetMax(char a, char b)
+        {
+            return a > b ? a : b;
+        }
+
+        static int GetMax(int a, int b)
+        {
+            return a > b ? a : b;
         }
     }
 }
