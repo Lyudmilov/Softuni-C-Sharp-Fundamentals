1c4392b905a80359ac14d1afdce8bc1d9bc20ca3
Dimitar Georgiev <dimitar.lyudmilov@gmail.com>
2023-10-17 01:24:02 +0300

    adding tasks

--- Lab Methods/11.MathOperations/Program.cs
+++ Lab Methods/11.MathOperations/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            
+            int num1 = int.Parse(Console.ReadLine());
+            char sign = char.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+           Console.WriteLine(CalculateOperation(num1, num2, sign));
+
+        }
+
+        static int CalculateOperation(int n1, int n2, char s)
+        {
+            int result = 0;
+
+            switch (s)
+            {
+                case '+':
+                    result = n1 + n2;
+                    break;
+                case '-':
+                    result = n1 - n2;
+                    break;
+                case '*':
+                    result = n1 * n2;
+                    break;
+                case '/':
+                    result = n1 / n2;
+                    break;
+            }
+
+            return result;
         }
     }
 }
