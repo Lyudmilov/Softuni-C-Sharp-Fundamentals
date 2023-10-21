b01bc393b0e8e907f7193e36a79fa7494af97137
Dimitar Georgiev <dimitar.lyudmilov@gmail.com>
2023-10-21 03:32:44 +0300

    Problem3 100/100

--- 19. Exam Preparation 1/3.Problem3–MovingTarget/Program.cs
+++ 19. Exam Preparation 1/3.Problem3–MovingTarget/Program.cs
@@ -11,19 +11,81 @@
                Strike 2 1
                Add 22 3
                End
-
-               52 74 23 44 96 110
-               Shoot 5 10
-               Shoot 1 80
-               Strike 4 3
-               Add 22 3
-               End
-
+                           
                1 2 3 4 5
                Strike 0 1
                End               
             */
+
+            //Решение 100/100 (Разликата с другото решение че тук проверявам при всяка команда, дали подадените стойности влизат
+            //в големината на масива, а при дугото решение първо проверявам дали влизат в масива и след това командите и се чупи някъде)
             List<int> targets = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string comands = "";
+            while ((comands = Console.ReadLine()) != "End")
+            {
+                List<string> inputValues = comands
+                    .Split()
+                    .ToList();
+                string comand = inputValues[0];
+                int index = int.Parse(inputValues[1]);
+                int value = int.Parse(inputValues[2]);
+
+                if (comand == "Shoot")
+                {
+                    if (index >= 0 && index < targets.Count)
+                    {
+                        targets[index] -= value;
+                        if (targets[index] <= 0)
+                        {
+                            targets.RemoveAt(index);
+                        }
+                    }
+                }
+                else if (comand == "Add")
+                {
+                    if (index >= 0 && index < targets.Count)
+                    {
+                        targets.Insert(index, value);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid placement!");
+                    }
+                }
+                else if (comand == "Strike")
+                {
+                    if (index >= 0 && index < targets.Count && index - value >= 0 && index + value < targets.Count)
+                    {
+                         targets.RemoveRange((index - value), 2 * value + 1);
+                       
+                        //Съюотот решение но с цикъл
+                        /* targets.RemoveAt(index);
+                        //left
+                        for (int i = 0; i < value; i++)
+                        {
+                            targets.RemoveAt(index + i);
+                        }
+                        //right
+                        for (int i = 1; i <= value; i++)
+                        {
+                            targets.RemoveAt(index - i);
+                        }*/
+                    }
+                    else
+                    {
+                        Console.WriteLine("Strike missed!");
+                    }
+                }                              
+            }
+            Console.WriteLine(string.Join("|", targets));
+
+
+            //Решение 80/100
+            /* List<int> targets = Console.ReadLine()
                 .Split()
                 .Select(int.Parse)
                 .ToList();
@@ -47,6 +109,7 @@
                         {
                             targets.RemoveAt(index);
                         }
+                        //Console.WriteLine(string.Join("|", targets));
                     }
                     else if (comand == "Add")
                     {
@@ -56,11 +119,22 @@
                     {
                         if (index >= (value - index) && index < (value + index))
                         {
-                            //Console.WriteLine($"index - {value}: {index - value}, index: {index}, index + {value}:{(value + index)}");
-                            targets.RemoveRange((index - value), 2 * value + 1);
+                            // също работи
+                            //targets.RemoveRange((index - value), 2 * value + 1);
+                            targets.RemoveAt(index);
+                            //left
+                            for (int i = 0; i < value; i++)
+                            {
+                                targets.RemoveAt(index + i);
+                            }
+                            //right
+                            for (int i = 1; i <= value; i++)
+                            {
+                                targets.RemoveAt(index - i);
+                            }
                         }
                         else if (index < (value - index)
-                                 && index > (value + index))
+                                 || index > (value + index))
                         {
                             Console.WriteLine("Strike missed!");
                         }
@@ -78,7 +152,7 @@
                     }
                 }
             }
-            Console.WriteLine(string.Join("|", targets));
+            Console.WriteLine(string.Join("|", targets));*/
         }
     }
 }
