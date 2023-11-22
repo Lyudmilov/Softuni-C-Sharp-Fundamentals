fa17922b4b83a0430b1e0aca2dfc6254332320fe
Dimitar Georgiev <dimitar.lyudmilov@gmail.com>
2023-11-22 18:32:21 +0200

    adding tasks

--- 15. Methods Exercise/9.PalindromeIntegers/Program.cs
+++ 15. Methods Exercise/9.PalindromeIntegers/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input;
+
+            int originalNumber = 0;
+
+            int reversedNumber = 0;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                originalNumber = int.Parse(input);
+                int tempNum = originalNumber;
+
+                reversedNumber = 0;
+
+                while (tempNum > 0)
+                {
+                    int remainder = tempNum % 10;
+                    reversedNumber = reversedNumber * 10 + remainder;
+                    tempNum = tempNum / 10;
+                }
+
+                bool isPalindrome = (originalNumber == reversedNumber);
+
+                if (isPalindrome)
+                {
+                    Console.WriteLine(true);
+                }
+                else
+                {
+                    Console.WriteLine(false);
+                }
+            }
+
         }
     }
 }
